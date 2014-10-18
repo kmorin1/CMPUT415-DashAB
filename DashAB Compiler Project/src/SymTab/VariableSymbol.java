8707d5d459799131ff260c4b016eee1b6c19b1ca
Lyle Rolleman <lrollema@ualberta.ca>
2014-10-18 10:10:00 -0600

    ST const spec

--- DashAB Compiler Project/src/SymTab/VariableSymbol.java
+++ DashAB Compiler Project/src/SymTab/VariableSymbol.java
@@ -1,10 +1,17 @@
 package SymTab;
 
 public class VariableSymbol extends Symbol {
+	boolean isconst;
 	public VariableSymbol(String name, Type type) { 
 		super(name, type); 
+		isconst = false;
+	}
+	public VariableSymbol(String name, Type type, boolean isconst) {
+		super(name, type);
+		this.isconst = isconst;
 	}
 	
-	
+	public boolean isConst() {return this.isconst;}
+	//public void setConst(boolean con) {this.isconst = con;}
 	public String toString() {return super.getName() + " " + super.getType();}
 }
