1a366e943c60f947f3bc1da5b57f460605718e59
stankiev <stankiev@ug18.cs.ualberta.ca>
2014-11-04 00:20:42 -0700

    Cleaned up DashAB_Test a bit

--- DashAB Compiler Project/src/dash/DashAB_Test.java
+++ DashAB Compiler Project/src/dash/DashAB_Test.java
@@ -35,76 +35,51 @@ public class DashAB_Test {
 			System.exit(1);
 		}
 
-		//try {
-			SymbolTable symtab = new SymbolTable();
-			SyntaxLexer lexer = new SyntaxLexer(input);
-			TokenStream tokenStream = new CommonTokenStream(lexer);
-			SyntaxParser parser = new SyntaxParser(tokenStream);
-			SyntaxParser.program_return entry = parser.program();
-			CommonTree ast = (CommonTree)entry.getTree();
-			DOTTreeGenerator gen = new DOTTreeGenerator();
-			StringTemplate st = gen.toDOT(ast);
-			//System.out.println(st);
+		SymbolTable symtab = new SymbolTable();
+		SyntaxLexer lexer = new SyntaxLexer(input);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		SyntaxParser parser = new SyntaxParser(tokenStream);
+		SyntaxParser.program_return entry = parser.program();
+		CommonTree ast = (CommonTree)entry.getTree();
+		DOTTreeGenerator gen = new DOTTreeGenerator();
+		StringTemplate st = gen.toDOT(ast);
+		//System.out.println(st);
 			
-			CommonTreeNodeStream nodes = new CommonTreeNodeStream(ast);
-			nodes.setTokenStream(tokenStream);
-			TypeExpand te = new TypeExpand(nodes, symtab, inputfile);
-			TypeExpand.program_return teret = te.program();
-			ast = (CommonTree) teret.getTree();
-			st = gen.toDOT(ast);
-			//System.out.println(st);
+		CommonTreeNodeStream nodes = new CommonTreeNodeStream(ast);
+		nodes.setTokenStream(tokenStream);
+		TypeExpand te = new TypeExpand(nodes, symtab, inputfile);
+		TypeExpand.program_return teret = te.program();
+		ast = (CommonTree) teret.getTree();
+		st = gen.toDOT(ast);
+		//System.out.println(st);
 			
-			nodes = new CommonTreeNodeStream(ast);
-			nodes.setTokenStream(tokenStream);
-			TypeTranslate tt = new TypeTranslate(nodes, symtab, inputfile);
-			TypeTranslate.program_return ttret = tt.program();
-			ast = (CommonTree) ttret.getTree();
+		nodes = new CommonTreeNodeStream(ast);
+		nodes.setTokenStream(tokenStream);
+		TypeTranslate tt = new TypeTranslate(nodes, symtab, inputfile);
+		TypeTranslate.program_return ttret = tt.program();
+		ast = (CommonTree) ttret.getTree();
 			
-			st = gen.toDOT(ast);
-			System.out.println(st);
+		st = gen.toDOT(ast);
+		System.out.println(st);
 
-			/*SymbolTable symtab = new SymbolTable();
-			
-			// Pass over to verify no variable misuse
-			CommonTreeNodeStream nodes = new CommonTreeNodeStream(ast);
+		String templateFile = "llvm.stg";
+
+		FileReader template;
+		try {
+		    template = new FileReader(templateFile);
+
+		    StringTemplateGroup stg = new StringTemplateGroup(template);
+
+		    nodes = new CommonTreeNodeStream(ast);
 		    nodes.setTokenStream(tokenStream);
-		    Defined defined = new Defined(nodes, symtab);
-		    defined.program();
-			
-			if (args[1].equals("int")) {
-				// Run it through the Interpreter
-				nodes.reset();
-				Interpreter interpreter = new Interpreter(nodes, symtab);
-				interpreter.program();
-			} else {
-				// Pass it all to the String templater!
-				 */
-				String templateFile = "llvm.stg";
 
-				FileReader template;
-				try {
-					template = new FileReader(templateFile);
-
-					StringTemplateGroup stg = new StringTemplateGroup(template);
-
-					nodes = new CommonTreeNodeStream(ast);
-		                        nodes.setTokenStream(tokenStream);
-
-					LLVMTemplater templater = new LLVMTemplater(nodes, symtab);
-					templater.setTemplateLib(stg);
-					System.out.println(templater.program().getTemplate().toString());				
-				} catch (FileNotFoundException e) {
-					System.out.print("The template file is missing:");
-					System.out.println(templateFile);
-				}
-			
-		/*} catch (RuntimeException e) {
-			System.out.println("A problem has occured with the DashAB input file: " + e.getMessage());
-			System.out.println("Please check the input file for correctness.");
-			e.printStackTrace();
-			System.exit(1);
-			
-		}*/
+		    LLVMTemplater templater = new LLVMTemplater(nodes, symtab);
+		    templater.setTemplateLib(stg);
+		    System.out.println(templater.program().getTemplate().toString());				
+		} catch (FileNotFoundException e) {
+		    System.out.print("The template file is missing:");
+		    System.out.println(templateFile);
+		}
 	}
 	
 }
