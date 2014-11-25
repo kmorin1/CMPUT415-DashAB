74bf0bb3e758ab142569737c8d63e588afacde06
Lyle Rolleman <lrollema@ualberta.ca>
2014-11-24 17:10:06 -0700

    AST subtree stuff in vector symbols

--- DashAB Compiler Project/src/SymTab/VectorTypeSymbol.java
+++ DashAB Compiler Project/src/SymTab/VectorTypeSymbol.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 
 public class VectorTypeSymbol extends BuiltInTypeSymbol {
 	Type vectype;
+	Object typetree;
 	Object vecsize;
 	public VectorTypeSymbol(String name) {
 		this(name, null, null);
 	}
-	public VectorTypeSymbol(String name, Type vectype) {
-		this(name, vectype, null);
+	public VectorTypeSymbol(String name, Type vectype, Object typetree) {
+		this(name, vectype, typetree, null);
 	}
-	public VectorTypeSymbol(String name, Type vectype, Object vecsize) {
+	public VectorTypeSymbol(String name, Type vectype, Object typetree, Object vecsize) {
 		super(name);
 		this.vectype = vectype;
+		this.typetree = typetree;
 		this.vecsize = vecsize;
 	}
 	public Type getVectorType() {return vectype;}
+	public Object getTypeTree() {return typetree;}
 	public Object getVectorSize() {return vecsize;}
 }
