0aa51c66da7187f69a4911413e53d0a53049cadb
Tsz Chung Chang <tchang2@ualberta.ca>
2014-12-02 19:59:55 -0700

    Tester - update

--- DashAB Compiler Project/src/automated_test/Test_thread.java
+++ DashAB Compiler Project/src/automated_test/Test_thread.java
@@ -1,21 +1,28 @@
 package automated_test;
 
 public class Test_thread extends Thread{
-	
+
 	private int start, end;
-	
+
 	public Test_thread(int s, int e){
 		this.start = s;
 		this.end = e;
 	}
-	
+
 	@Override
 	public void run(){
 		System.out.println("This is thread - " + this.getId() + " processing range " + this.start + "-" + this.end);
-		
+
 		for(int i = start; i <= end; i++){
-			if(Tester.llc_test == 1)
-				Test_loader.llc_result.add(Test_llc.run(Test_loader.tests.get(i),this.getId()));
+			int result = 0;
+			if(Tester.llc_test == 1){
+				result = Test_llc.run(Test_loader.tests.get(i),this.getId());
+				Test_loader.llc_result.add(result);
+				if(result == 0)
+					Test_loader.llc_result_works.add(Test_loader.tests.get(i));
+				if(result == -1)
+					Test_loader.llc_result_nope.add(Test_loader.tests.get(i));	
+			}
 			if(Tester.dash_test == 1)
 				Test_loader.dash_result.add(Test_dash.run(Test_loader.tests.get(i),this.getId()));
 			try {
@@ -25,8 +32,8 @@ public class Test_thread extends Thread{
 				e.printStackTrace();
 			}
 		}
-		
-		
+
+
 	}
 
 }
